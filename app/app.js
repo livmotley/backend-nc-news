be9ef716f2c503600cb5c0f3a2be904bb49dd93c
Liv Motley <livmotley.uk@gmail.com>
2025-03-10 11:45:06 +0000

    GET /api/articles/:article_id | Q3 complete with testing

--- app/app.js
+++ app/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
-const { getAllEndpoints, getAllTopics } = require("../controllers/api.controllers");
-const { handleServerErrors } = require("../controllers/errors.controllers");
+const { getAllEndpoints } = require('../controllers/api.controllers.js')
+const { getAllTopics } = require("../controllers/topics.controllers.js");
+const { getArticleById } = require('../controllers/articles.controllers')
+const { handleServerErrors, handleCustomErrors, handlePsqlErrors } = require("../controllers/errors.controllers");
 
 app.get('/api', getAllEndpoints);
 
 app.get('/api/topics', getAllTopics);
+
+app.get('/api/articles/:article_id', getArticleById);
+
+app.use(handleCustomErrors);
+
+app.use(handlePsqlErrors);
 
 app.use(handleServerErrors);
 
--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -1,16 +1,5 @@
 const endpoints = require("../endpoints.json");
-const { fetchAllTopics } = require('../models/api.models');
 
 exports.getAllEndpoints = (req, res) => {
     res.status(200).send({endpoints});
-}
-
-exports.getAllTopics = (req, res, next) => {
-    fetchAllTopics()
-    .then((topics) => {
-        res.status(200).send({ topics })
-    })
-    .catch((err) => {
-        next(err);
-    })
 }
