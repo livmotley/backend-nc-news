511e7d69bf16e7adc50810382bf06a125ef7f557
Liv Motley <livmotley.uk@gmail.com>
2025-03-11 15:12:18 +0000

    initial setup of hosting

--- app/listen.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app.js');
-
-app.listen(8080, (err) => {
-    err ? console.log(err) : console.log('listening on port 8080');
-})
--- db/connection.js
+++ db/connection.js
@@ -6,10 +6,17 @@ require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
 const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not configured")
 } else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
+}
+
+const config = {};
+
+if(ENV === 'production') {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
 }
 
 
